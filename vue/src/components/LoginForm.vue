c0da6577f71e087e71745c65bd093f3f3f30a82e
yanghj <gmlwls0522@gmail.com>
2024-07-10 21:46:32 +0900

    vue 메인 페이지 변경

---
+++ vue/src/components/LoginForm.vue
@@ -0,0 +1,8 @@
+<script setup>
+import { reactive } from 'vue'
+
+</script>
+
+<template>
+  <div></div>
+</template>
---
+++ vue/src/views/Main.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1>hello world!</h1>
+</template>
