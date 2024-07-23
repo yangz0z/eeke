641271c5390c25c9c237fd7505c625c9e839bf0c
yanghj <gmlwls0522@gmail.com>
2024-07-23 10:07:46 +0900

    새로고침 시에도 userStore 값 유지(vue-persistedsate)

--- frontend/src/stores/index.js
+++ frontend/src/stores/index.js
@@ -1,22 +1,14 @@
 import { createStore } from 'vuex'
 import userStore from './modules/userStore'
+import createPersistedState from 'vuex-persistedstate';
 
 const store = createStore({
-    state: {
-        
-    },
-    mutations: {
-        
-    },
-    getters: {
-        
-    },
-    actions: {
-        
-    },
     modules: {
         user: userStore
-    }
+    },
+    plugins: [createPersistedState({
+        paths: ['user']
+    })]
 })
 
 export default store
